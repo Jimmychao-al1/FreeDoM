bc02705f89f71c56bdb32d018ebc5f32c29acbe7
Jimmychao-al1 <79821770+Jimmychao-al1@users.noreply.github.com>
2023-11-17 11:22:53 +0000

    update

--- convert.py
+++ convert.py
@@ -1,9 +1,19 @@
 import yaml
 
-with open("environment.yaml") as file_handle:
-    environment_data = yaml.load(file_handle)
+yaml = yaml.YAML()
+data = yaml.load(open('environment.yml'))
 
-with open("requirements.txt", "w") as file_handle:
-    for dependency in environment_data["dependencies"]:
-        package_name, package_version = dependency.split("=")
-        file_handle.write("{} == {}".format(package_name, package_version))
+requirements = []
+for dep in data['dependencies']:
+    if isinstance(dep, str):
+        package, package_version, python_version = dep.split('=')
+        if python_version == '0':
+            continue
+        requirements.append(package + '==' + package_version)
+    elif isinstance(dep, dict):
+        for preq in dep.get('pip', []):
+            requirements.append(preq)
+
+with open('requirements.txt', 'w') as fp:
+    for requirement in requirements:
+       print(requirement, file=fp)
