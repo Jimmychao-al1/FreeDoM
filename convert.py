a214fccd71f801c71adf6a8c563be2f0d481e7ba
Jimmychao-al1 <79821770+Jimmychao-al1@users.noreply.github.com>
2023-11-17 11:20:17 +0000

    update

---
+++ convert.py
@@ -0,0 +1,9 @@
+import yaml
+
+with open("environment.yaml") as file_handle:
+    environment_data = yaml.load(file_handle)
+
+with open("requirements.txt", "w") as file_handle:
+    for dependency in environment_data["dependencies"]:
+        package_name, package_version = dependency.split("=")
+        file_handle.write("{} == {}".format(package_name, package_version))
